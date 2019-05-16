73f582ab11deee7a320a901d8ea3bdfcc4f94bb4
chengwj <chengwjn@126.com>
2019-05-16 20:50:51 +0800

    SocketTransfer
    基于Socket使用tcp进行传输的工具

---
+++ SocketConnect/Server/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnListen = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lsbMessage = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(73, 55);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(139, 21);
+            this.txtIP.TabIndex = 0;
+            this.txtIP.Text = "127.0.0.1";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(316, 55);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(149, 21);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "50001";
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(73, 347);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(289, 21);
+            this.txtSend.TabIndex = 3;
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(495, 55);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(75, 23);
+            this.btnListen.TabIndex = 4;
+            this.btnListen.Text = "开始监听";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.BtnListen_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(495, 347);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(257, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Port";
+            // 
+            // lsbMessage
+            // 
+            this.lsbMessage.FormattingEnabled = true;
+            this.lsbMessage.ItemHeight = 12;
+            this.lsbMessage.Location = new System.Drawing.Point(73, 98);
+            this.lsbMessage.Name = "lsbMessage";
+            this.lsbMessage.Size = new System.Drawing.Size(448, 232);
+            this.lsbMessage.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 450);
+            this.Controls.Add(this.lsbMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnListen);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lsbMessage;
+    }
+}
+
