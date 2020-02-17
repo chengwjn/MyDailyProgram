9f82360b4ffa7950ed15cea48d3c0ce6205cf6fb
chengwj <chengwjn@126.com>
2020-02-17 16:15:59 +0800

    Add files via upload

---
+++ PythonStudy/20200215/randomTest.py
@@ -0,0 +1,12 @@
+import random
+x = random.getstate()
+print(x)
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+random.setstate(x)
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
