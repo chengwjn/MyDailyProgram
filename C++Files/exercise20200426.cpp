0cd606ff49737314d5c69d9facf55593eb2f1471
chengwj <chengwjn@126.com>
2020-04-26 21:02:23 +0800

    Add files via upload

---
+++ C++Files/exercise20200426.cpp
@@ -0,0 +1,39 @@
+// #include <iostream>
+// int main()
+// {
+//     using namespace std;
+//     char flower[10] = "rose";
+//     cout << flower << "s are red\n";
+// }
+//using pointers to strings
+#include <iostream>
+#include <cstring>
+int main()
+{
+    using namespace std;
+    char animal[20] = "bear";  //animal holds bear
+    const char *bird = "wren"; //bird holds address of string
+    char *ps;                  //uninitialized
+
+    cout << animal << " and "; //display bear
+    cout << bird << "\n";      //display wren
+    //cout << ps <<"\n"; //may display garbage ,may cause a crash
+
+    cout << "Enter a kind of animal:";
+    cin >> animal; //ok if input < 20 chars
+    //cin>>ps; too horrible a blunder to try ;ps doesn't
+    //point to allocated space
+
+    ps = animal; //set ps to point to string
+    cout << "Before using strcpy();\n";
+    cout << animal << " at " << (int *)animal << endl;
+    cout << ps << " at " << (int *)ps << endl;
+
+    ps = new char[strlen(animal) + 1]; //get new storage
+    strcpy(ps, animal);                //copy string to new storage
+    cout << "After using strcpy();\n";
+    cout << animal << " at " << (int *)animal << endl;
+    cout << ps << " at " << (int *)ps << endl;
+    delete[] ps;
+    return 0;
+}
