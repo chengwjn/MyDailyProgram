6c8e20503bf69f7ce07b6db02aa108b10bb3e730
ChengWenjie <chengwjn@126.com>
2020-05-11 20:55:08 +0800

    Update lotto.cpp

--- C++Files/Cpp_202005/lotto.cpp
+++ C++Files/Cpp_202005/lotto.cpp
@@ -16,3 +16,14 @@ int main()
     cout << "bye\n";
     return 0;
 }
+long double probability(unsigned numbers, unsigned picks)
+{
+    long double result = 1.0;
+    long double n;
+    unsigned p;
+    for (n = numbers, p = picks; p > 0; n--, p--)
+    {
+        result = result * n / p;
+    }
+    return result;
+}
