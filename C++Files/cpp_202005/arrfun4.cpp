53d632ad3b8152cc327b5f200ef6a1b9e7039476
ChengWenjie <chengwjn@126.com>
2020-05-11 23:20:45 +0800

    Update arrfun4.cpp

---
+++ C++Files/cpp_202005/arrfun4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+const int Arsize = 8;
+int sum_arr(const int *begin, const int *end);
+int main()
+{
+    using namespace std;
+    int cookies[Arsize] = {1, 2, 4, 8, 16, 32, 64, 128};
+    int sum = sum_arr(cookies, cookies + Arsize);
+    //int a=cookies+Arsize;
+    cout << "Total cookies eaten: " << sum << endl;
+    sum = sum_arr(cookies, cookies + 3);
+    cout << "First three eaters ate " << sum << " cookies.\n";
+    sum = sum_arr(cookies + 4, cookies + 8);
+    cout << "Last four eaters ate " << sum << " cookies.\n";
+    return 0;
+}
+int sum_arr(const int *begin, const int *end)
+{
+    const int *pt;
+    int total;
+    for (pt = begin; pt != end; pt++)
+    {
+        total = total + *pt;
+    }
+    return total;
+}
