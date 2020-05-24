2d863bb219bee417ae00c280727b987a35a16c62
ChengWenjie <chengwjn@126.com>
2020-05-24 17:59:41 +0800

    Update strctptr.cpp

--- C++Files/cpp_202005/strctptr.cpp
+++ C++Files/cpp_202005/strctptr.cpp
@@ -35,6 +35,16 @@ int main()
 void show_polar(const polar *pda)
 {
     using namespace std;
-    pda->distance=sqrt(pxy->x*pxy->x+pxy->y*pxy->y);
-    pda->angle=atan2(pxy->x,pxy->x);
+    const double Rad_to_deg = 57.29577951;
+
+    cout << "distance = " << pda->distance;
+    cout << ", angel = " << pda->angle * Rad_to_deg;
+    cout << " degrees\n";
+}
+//convert rectangular to polar coordinates
+void rect_to_polar(const rect *pxy, polar *pda)
+{
+    using namespace std;
+    pda->distance = sqrt(pxy->x * pxy->y + pxy->y * pxy->y);
+    pda->angle = atan2(pxy->y, pxy->x);
 }
