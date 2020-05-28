8e538ffeb4c6a6e434c1ab48a86b1fe288a2542c
ChengWenjie <chengwjn@126.com>
2020-05-28 12:37:55 +0800

    Create ruler.cpp

---
+++ C++Files/cpp_202005/ruler.cpp
@@ -0,0 +1,25 @@
+//ruler.cpp -- using recursion to subdivide a ruler
+#include <iostream>
+const int Len = 66;
+const int Divs = 6;
+void subdivide(char ar[], int low, int high, int level);
+int main()
+{
+    char ruler[Len];
+    int i;
+    for (i = 1; i < Len; i++)
+        ruler[i] = ' ';
+    ruler[Len - 1] = '\0';
+    int max = Len - 2;
+    int min = 0;
+    ruler[min] = ruler[max] = '|';
+    std::cout << ruler << std::endl;
+    for (i = 1; i <= Divs; i++)
+    {
+        subdivide(ruler, min, max, i);
+        std::cout << ruler << std::endl;
+        for (int j = 1; j < Len - 2; j++)
+            ruler[j] = ' '; //reset to blank ruler
+    }
+    return 0;
+}
