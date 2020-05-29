75858c80bed4722567dd703e446adf0bb6596968
ChengWenjie <chengwjn@126.com>
2020-05-29 19:17:52 +0800

    Update firstref.cpp

---
+++ C++Files/cpp_202005/firstref.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int rats = 101;
+    int &rodents = rats; //rodents is a reference
+    cout << "rats = " << rats;
+    cout << ",rodents = " << rodents << endl;
+    rodents++;
+    cout << "rats = " << rats;
+    cout << ", rodents = " << rodents << endl;
+
+    //some implementations require type casting the following
+    //addressto type unsigned
+    cout << "rats address = " << &rats;
+    cout << ",rodents address = " << &rodents << endl;
+    return 0;
+}
