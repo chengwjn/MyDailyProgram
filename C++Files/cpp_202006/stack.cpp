c1844b59bc583ba38b67f0aaff79d03e5f2c3c43
chengwj <chengwjn@126.com>
2020-06-17 12:11:28 +0800

    Update stack.cpp

--- C++Files/cpp_202006/stack.cpp
+++ C++Files/cpp_202006/stack.cpp
@@ -9,18 +9,18 @@ bool Stack::push(const Item&item)
 {
 		if(top<MAX)
 		{
-				items[top++] = item;
-				return true;
+			items[top++] = item;
+			return true;
 		}
 		else
-				return false;
+			return false;
 }
 bool Stack::pop(Item &item)
 {
 		if(top>0)
 		{
-				 item = items[--top];
-				 return true;
+			item = items[--top];
+			return true;
 		}
 		else
 		return false;
