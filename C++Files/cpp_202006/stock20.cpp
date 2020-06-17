eb4bccaa1eb192f5d3ab73493c026cef891d9289
ChengWenjie <chengwjn@126.com>
2020-06-17 18:36:35 +0800

    Update stock20.cpp

--- C++Files/cpp_202006/stock20.cpp
+++ C++Files/cpp_202006/stock20.cpp
@@ -1,82 +1,88 @@
 //stock20.cpp -- argumented version
-#include<iostream>
+#include <iostream>
 #include "stock20.h"
-Stock::Stock()//default constructor
+Stock::Stock() //default constructor
 {
-	company ="no name";
-	shares=0;
-	share_val=0.0;
-	total_val=0.0;
+	company = "no name";
+	shares = 0;
+	share_val = 0.0;
+	total_val = 0.0;
 }
-Stock::Stock(const std::string & co,long n,double pr)
+Stock::Stock(const std::string &co, long n, double pr)
 {
-	company =co;
-	if(n<0)
+	company = co;
+	if (n < 0)
 	{
-		std::cout<<"Number of shares can't be negative;"
-		<<company <<"shares set to 0.\n";
-		shares=0
+		std::cout << "Number of shares can't be negative;"
+				  << company << "shares set to 0.\n";
+		shares = 0
 	}
 	else
-		shares=n;
-	share_val=pr;
+		shares = n;
+	share_val = pr;
 	set_tot();
 }
 
 //class destructor
-Stock::~Stock()//quiet class destructor
-{}
+Stock::~Stock() //quiet class destructor
+{
+}
 
 //other methods
-void Stock::buy(long num,double price)
+void Stock::buy(long num, double price)
 {
-	if(num<0)
+	if (num < 0)
 	{
-		std::cout<<"Number of shares purchased can't be negative."<<"transaction is aborted.\n";
+		std::cout << "Number of shares purchased can't be negative."
+				  << "transaction is aborted.\n";
 	}
-	elser
+	else
 	{
-		shares+=num;
-		share_val=price;
+		shares += num;
+		share_val = price;
 		set_tot;
 	}
 }
-void Stock::sell(long num,double price)
+void Stock::sell(long num, double price)
 {
 	using std::cout;
-	if(num<0)
+	if (num < 0)
 	{
-		cout<<"Number of shares sold can't be negative."<<"transaction is aborted.\n";
+		cout << "Number of shares sold can't be negative."
+			 << "transaction is aborted.\n";
 	}
-	else if(num>shares)
+	else if (num > shares)
 	{
-		cout<<"You can't sell more than you have!"
-		<<"Transaction is aborted.\n";
+		cout << "You can't sell more than you have!"
+			 << "Transaction is aborted.\n";
 	}
-	else{
-		shares-=num;
-		share_val=price;
+	else
+	{
+		shares -= num;
+		share_val = price;
 		set_tot();
 	}
 }
 
-void Stock::show() const{
+void Stock::show() const
+{
 	using std::cout;
 	using std::ios_base;
-	ios_base::fmtflags orig=cout.setf(ios_base::fixed,ios_based::floatfield);
-	std::streamsize prec =cout,precision(3);
-	
-	cout<<"Company:"<<company<<" shares:"<<shares<<"\n";
-	cout<<" Share Price:$ :<<share_val;
-	cout.precision(2);
-	cout<<" Total Worth:$"<<total_val<<'\n';
-	
+	ios_base::fmtflags orig = cout.setf(ios_base::fixed, ios_base::floatfield);
+	std::streamsize prec = cout.precision(3);
+
+	cout << "Company:" << company << " shares:" << shares << "\n";
+	cout << " Share Price:$ :<<share_val"
+		 << cout.precision(2);
+	cout << " Total Worth:$" << total_val << '\n';
+
 	//restore original format
-	cout.setf(orig,ios_base::floatfoeld);
+	cout.setf(orig, ios_base::floatfield);
 	cout.precision(prec);
 }
-const Stock &Stock::topval(const Stock &s)const{
-	if(s.total_val >total_val)
+const Stock &Stock::topval(const Stock &s) const
+{
+	if (s.total_val > total_val)
 		return 0;
 	else
 		return *this;
