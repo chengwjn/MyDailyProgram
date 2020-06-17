1c7f22e0de16bfb070223cff85bf6163427f3c03
ChengWenjie <chengwjn@126.com>
2020-06-17 18:33:04 +0800

    Update stock00.h

--- C++Files/cpp_202006/stock00.h
+++ C++Files/cpp_202006/stock00.h
@@ -9,21 +9,22 @@
 #ifndef stock00_h
 #define stock00_h
 
-#include<string>
+#include <string>
 class Stock
 {
 private:
     std::string company;
     long shares;
     double share_val;
-    void total_val;
-    void set_tot(){total_val=shares*share_val;}
+    double total_val;
+    void set_tot() { total_val = shares * share_val; }
+
 public:
-    void acquire(const std::string &co,long n,double pr);
-    void buy(long num,double price);
-    void sell(long num,double price);
+    void acquire(const std::string &co, long n, double pr);
+    void buy(long num, double price);
+    void sell(long num, double price);
     void update(double price);
     void show();
-};//note semicolon at the end
+}; //note semicolon at the end
 
 #endif /* stock00_h */
