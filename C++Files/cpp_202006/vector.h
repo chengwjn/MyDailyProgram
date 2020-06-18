2dd01d09134ca956e15533eb9a9ea840535a15bb
ChengWenjie <chengwjn@126.com>
2020-06-18 19:18:48 +0800

    Update Something

---
+++ C++Files/cpp_202006/vector.h
@@ -0,0 +1,32 @@
+#ifndef VECTOR_H_
+#define VECTOR_H_
+#include<iostream>
+namespace VECTOR
+{
+    class Vector
+    {
+        public :
+        enum Mode{RECT,POL};
+        //RECT for rectangular,POL for Polar modes
+        private :
+        double x;//horizontal value
+        double y;//vertical value
+        double mag;//length of vector
+        double ang;//direction of vector in degree
+        Mode mode;//RECT or POL
+        //private methods for setting values
+        void set_mag();
+        void set_ang();
+        void set_x();
+        void set_y();
+        public :
+        Vector();
+        Vector(double n1,double n2,Mode form =RECT);
+        void reset(double n1,double n2,Mode form=RECT);
+        ~Vector();
+        double xval() const{return x;}//report x value
+        double yval() const {return y;}
+
+    };
+}
+#endif
