8f92ef05a6f13d39fc8aadb7400c5512908bc4a5
ChengWenjie <chengwjn@126.com>
2020-06-28 20:59:28 +0800

    tabtenn1.h

--- C++Files/cpp_202006/tabtenn1.h
+++ C++Files/cpp_202006/tabtenn1.h
@@ -25,7 +25,7 @@ private:
     unsigned int rating;
 
 public:
-    RatedPlayer(unsigned int r = 0, const string &fn = "none", const string &ln = "none", bool TableTennisPlayer &tp);
+    RatedPlayer(unsigned int r = 0, const string &fn = "none", const string &ln = "none", bool ht = false);
     RatedPlayer(unsigned int r, const TableTennisPlayer &tp);
     unsigned int Rating() const { return rating; }
     void ResetRating(unsigned int r) { rating = r; }
