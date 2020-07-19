43da3462192e5186fa8abd113140702e6a7223b8
ChengWenjie <chengwjn@126.com>
2020-07-20 00:05:41 +0800

    Update acctabc.h

---
+++ C++Files/cpp_202007/acctabc.h
@@ -0,0 +1,19 @@
+#ifndef ACCTABC_H_
+#define ACCTABC_H_
+#include<iostream>
+#include<string>
+
+//Abstract Base Class
+class Acctabc
+{
+    private:
+    std::string fullname;
+    long acctNUm;
+    double balance;
+    protected:
+    
+
+}
+
+
+#endif
