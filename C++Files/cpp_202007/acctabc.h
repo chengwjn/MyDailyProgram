b56e4571f32d6d94a2416b9d572eec558b68e8cc
ChengWenjie <chengwjn@126.com>
2020-07-20 20:26:28 +0800

    acctabc.h

--- C++Files/cpp_202007/acctabc.h
+++ C++Files/cpp_202007/acctabc.h
@@ -1,22 +1,34 @@
 #ifndef ACCTABC_H_
 #define ACCTABC_H_
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 //Abstract Base Class
 class Acctabc
 {
-    private:
+private:
     std::string fullname;
-    long acctNUm;
+    long acctNum;
     double balance;
-    protected:
+
+protected:
     struct Formatting
     {
         std::ios_base::fmtflags flag;
-    }
-    
-}
+        std::streamsize pr;
+    };
+    const std::string &Fullname() const { return fullname; }
+    long AccNum() const { return acctNum; }
+    Formatting SetFormat() const;
+    void Restore(Formatting &f) const;
 
+public:
+    Acctabc(const std::string &s = "Nullbody", long an = -1, double bal = 0.0);
+    void Desposit(double amt);
+    virtual void Withdraw(double amt) = 0; //pure virtual function
+    double Balance() const { return balance; }
+    virtual void ViewAcct() const = 0; //pure virtual function
+    virtual ~Acctabc() {}
+};
 
 #endif
