20e6ffc8849d7bb7b3741b7a2d8b547bc71daf81
ChengWenjie <chengwjn@126.com>
2020-07-22 22:19:10 +0800

    acctabc.h

--- C++Files/cpp_202007/acctabc.h
+++ C++Files/cpp_202007/acctabc.h
@@ -30,5 +30,32 @@ public:
     virtual void ViewAcct() const = 0; //pure virtual function
     virtual ~Acctabc() {}
 };
+//Brass Account Class
+class Brass : public Acctabc
+{
+public:
+    Brass(const std::string &s = "Nullbody", long an = -1, double bal = 0.0) : Acctabc(s, an, bal) {}
+    virtual void Withdraw(double amt);
+    virtual void ViewAcct() const;
+    virtual ~Brass() {}
+};
+
+//Brass Plus Account Class
+class BrassPlus : public Acctabc
+{
+private:
+    double maxLoan;
+    double rate;
+    double owesBank;
+
+public:
+    BrassPlus(const std::string &S = "NUllbody", long an = -1, double bal = 0.0, double ml = 500, double r = 0.10);
+    BrassPlus(const Brass &ba, double ml = 500, double r = 0.1);
+    virtual void ViewAcct() const;
+    virtual void Withdraw(double amt);
+    void ResetMAx(double m) { maxLoan = m; }
+    void ResetRate(double r) { rate = r; };
+    void ResetOwes() { owesBank = 0; }
+};
 
 #endif
