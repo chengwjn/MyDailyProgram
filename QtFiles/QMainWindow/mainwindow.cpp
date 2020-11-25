a8956212b83ff1d000139ca0da3d6abe25fc3b13
chengwj <chengwjn@126.com>
2020-11-25 19:13:38 +0800

    20201125

---
+++ QtFiles/QMainWindow/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include <QAction>
+#include <QMenu>
+#include <QMenuBar> //菜单栏
+
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent)
+{
+    //菜单栏
+    QMenuBar* mBar = menuBar();
+    //工具栏
+    QMenu* pFile = mBar->addMenu("文件");
+    //添加菜单项,添加动作
+    QAction* pNew = pFile->addAction("新建");
+
+    //状态栏
+    //核心控件
+    //浮动窗口
+}
+
+MainWindow::~MainWindow()
+{
+}
